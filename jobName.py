1bb96d81b5908250b7337b20e6c6d62e21864073
blepfo <blepfo@gmail.com>
2016-09-30 18:03:45 +0000

    Updated with current copy of the repo on the pi

--- jobName.py
+++ jobName.py
@@ -6,23 +6,20 @@ from datetime import date
 from bs4 import BeautifulSoup
 
 # setup
-fname = "/home/pi/LabelMaker/label.html"
-#fname = "MakerBot Innovation Center.html"
+fname = "/home/pi/dymo/label.html"
 soup = BeautifulSoup(open(fname), 'html.parser')
-out = "/home/pi/LabelMaker/clipboard.txt"
-#out = "clipboard.txt"
+out = "/home/pi/dymo/clipboard.txt"
 
 #parse
 nameGetter = soup.find_all(text = re.compile("^Requestor"))[0].next
 jobNum = soup.find_all(text = re.compile("^Job"))[2].next.rstrip("\n")
 emailGetter = soup.find_all(text = re.compile("^Requestor Email"))[0].next.next.next_element
-#cost = soup.find_all(text = re.compile("^Cost"))[0].next
-today = str(date.today())
-
 try:
-    cost = soup.find_all(text = re.compile("^Cost"))[0].next
+  cost = soup.find_all(text = re.compile("^Cost"))[0].next
 except:
-    cost = soup.find_all(text = re.compile("^Estimated Cost"))[0].next
+  cost = soup.find_all(text = re.compile("^Estimated Cost"))[0].next
+
+today =str(date.today())
 
 #format
 nameSplitter = nameGetter.split()
@@ -36,4 +33,4 @@ f.writelines(lineOne)
 f.close()
 
 #send printer command
-os.system("lpr -P DYMO_LabelWriter_450_Turbo -o PageSize=Custom.20x80mm -o landscape /home/pi/LabelMaker/clipboard.txt")
+os.system("lpr -P DYMO_LabelWriter_450_Turbo -o PageSize=Custom.20x80mm -o landscape /home/pi/dymo/clipboard.txt") 
