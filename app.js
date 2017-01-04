3285834aae5f0263a636a1d9377609de3081ec65
blepfo <blepfo@gmail.com>
2017-01-04 20:54:49 +0000

    Version 1.0

--- app.js
+++ app.js
@@ -8,11 +8,16 @@ var http = require("http");
 var spawn = require('child_process').exec;
 
 // Create HTTP Server to listen for the Chrome Extension's request
-http.createServer(function(request, response) {
+http.createServer(onRequest).listen(9292);
+
+
+function onRequest(request, response) {
   console.log("Message Received");
+  console.log(request.method);
 
   // Make sure request is POST so it will have data
   if (request.method == 'POST') {
+  console.log("Post received");
     request.on('data', function(chunk) {
       fs.writeFile("/home/pi/dymo/label.html", chunk, function() {
         console.log("HTML saved to label.html");
@@ -26,4 +31,4 @@ http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Message Received\n");
   });
-}).listen(9292);
+}
